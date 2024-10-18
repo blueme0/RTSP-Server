d61446974c0693b1e44ee8d2c1231ab77a11b110
blueme0 <ksytpdus0304@gmail.com>
2024-10-18 18:06:01 +0900

    feat: update PathUtils to load files from the local device

--- app/src/main/java/com/pedro/sample/PathUtils.kt
+++ app/src/main/java/com/pedro/sample/PathUtils.kt
@@ -15,9 +15,15 @@
  */
 package com.pedro.sample
 
+import android.content.ContentUris
 import android.content.Context
+import android.database.Cursor
 import android.media.MediaScannerConnection
+import android.net.Uri
+import android.os.Build
 import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
 import java.io.File
 
 /**
@@ -35,5 +41,103 @@ object PathUtils {
   @JvmStatic
   fun updateGallery(context: Context, path: String) {
     MediaScannerConnection.scanFile(context, arrayOf(path), null, null)
+  }
+
+  @JvmStatic
+  fun getPath(context: Context, uri: Uri): String? {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && DocumentsContract.isDocumentUri(
+        context, uri
+      )) {
+      if (isExternalStorageDocument(uri)) {
+        val docId = DocumentsContract.getDocumentId(uri)
+        val split = docId.split(":")
+        val type = split[0]
+
+        var storageDefinition: String = ""
+
+        if ("primary".lowercase() == type.lowercase()) {
+          return "${Environment.getExternalStorageDirectory()}/${split[1]}"
+        } else {
+          if (Environment.isExternalStorageRemovable()) {
+            storageDefinition = "EXTERNAL_STORAGE"
+          } else {
+            storageDefinition = "SECONDARY_STORAGE"
+          }
+
+          return "${System.getenv(storageDefinition)}/${split[1]}"
+        }
+      } else if (isDownloadsDocuent(uri)) {
+        val id = DocumentsContract.getDocumentId(uri)
+        val contentUri = ContentUris.withAppendedId(
+          Uri.parse("content://downloads/public_downloads"),
+          id.toLong()
+        )
+
+        return getDataColumn(context, contentUri, null, null)
+      } else if (isMediaDocument(uri)) {
+        val docId = DocumentsContract.getDocumentId(uri)
+        val split = docId.split(":")
+        val type = split[0]
+
+        var contentUri: Uri? = null
+        if ("image" == type) {
+          contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        } else if ("video" == type) {
+          contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+        } else if ("audio" == type) {
+          contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+        }
+
+        val selection: String = "_id=?"
+        val selectionArgs: Array<String> = arrayOf(split[1])
+
+        return getDataColumn(context, contentUri, selection, selectionArgs)
+      }
+    } else if ("content" == uri.scheme?.lowercase()) {
+      if (isGooglePhotosUri(uri)) return uri.lastPathSegment
+      return getDataColumn(context, uri, null, null)
+    } else if ("file" == uri.scheme?.lowercase()) {
+      return uri.path
+    }
+    return null
+  }
+
+  @JvmStatic
+  fun getDataColumn(context: Context, uri: Uri?, selection: String?, selectionArgs: Array<String>?): String? {
+    var cursor: Cursor? = null
+    val column: String = "_data"
+    val projection: Array<String> = arrayOf(column)
+
+    if (uri == null) return null
+    try {
+      cursor = context.contentResolver.query(uri, projection, selection, selectionArgs, null)
+      if (cursor != null && cursor.moveToFirst()) {
+        val column_index = cursor.getColumnIndexOrThrow(column)
+        return cursor.getString(column_index)
+      }
+    } finally {
+      cursor?.close()
+    }
+    return null
+  }
+
+  @JvmStatic
+  fun isExternalStorageDocument(uri: Uri): Boolean {
+    return "com.android.externalstroage.documents" == uri.authority
+  }
+
+  @JvmStatic
+  fun isDownloadsDocuent(uri: Uri): Boolean {
+    return "com.android.providers.downloads.documents" == uri.authority
+  }
+
+  @JvmStatic
+  fun isMediaDocument(uri: Uri): Boolean {
+    return "com.android.providers.media.documents" == uri.authority
+  }
+
+  @JvmStatic
+  fun isGooglePhotosUri(uri: Uri): Boolean {
+    return "com.google.android.apps.photos.content" == uri.authority
   }
 }
